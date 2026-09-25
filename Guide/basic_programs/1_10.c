115ba25a513cb49a2b4911a4b90c0c51409d6df6
crazydev1990 <modiyashpal2@gmail.com>
2026-09-25 15:35:57 +0530

    Add files via upload

---
+++ Guide/basic_programs/1_10.c
@@ -0,0 +1,22 @@
+
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+void main()
+{
+    //clrscr();
+    int hh,mm,ss;
+    printf("\n Enter Time in HH::MM:");
+    scanf("%d:%d",&hh,&mm);
+    printf("%02d Hour And %02d Minutes",hh,mm);
+    getch();
+}
+
+
+
+
+
+
+
+
---
+++ Guide/basic_programs/1_11.c
@@ -0,0 +1,22 @@
+
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+int main()
+{
+    //clrscr();
+    int x=printf("CorexTrime");
+    printf("%d",x);
+    return 0;
+    //getch();
+}
+
+
+
+
+
+
+
+
+
